e7697b02c9694e30fd5661e3fc1cbc4b20e57416
ayush-763 <ayushchikate2003@gmail.com>
2024-12-17 17:00:19 +0530

    Creating Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import "./App.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 function App() {
-  return <>APP</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignOut />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/SignOut.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignOut = () => {
+  return <div>Sign Up</div>;
+};
+
+export default SignOut;
